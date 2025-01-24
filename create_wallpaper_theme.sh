a116ac2f2e6bc4e5062602b872d7a1d20b774924
xXMacMillanXx <xXMacMillanXx@gmx.at>
2025-01-24 23:20:44 +0100

    add readme and script

---
+++ create_wallpaper_theme.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+if [ $# -ne 6 ]; then
+    echo "usage: script.sh -n theme_name -l light_wallpaper -d dark_wallpaper"
+    exit 1
+fi
+
+while getopts n:l:d: flag
+do
+    case "${flag}" in
+        n) name=${OPTARG};;
+        l) light=${OPTARG};;
+        d) dark=${OPTARG};;
+    esac
+done
+
+themepath="/usr/share/gnome-background-properties/"
+customwp="/usr/share/backgrounds/custom/"
+lexp=$(echo "$light" | awk -F . '{print $NF}')
+dexp=$(echo "$dark" | awk -F . '{print $NF}')
+lightwp=$customwp$name-l.$lexp
+darkwp=$customwp$name-d.$dexp
+themefile=$themepath$name.xml
+
+mkdir -p $customwp
+cp "$light" "$lightwp"
+cp "$dark" "$darkwp"
+
+chmod +r-wx "$lightwp"
+chmod +r-wx "$darkwp"
+
+touch $themefile
+echo '<?xml version="1.0"?>' > $themefile
+echo '<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">' >> $themefile
+echo '<wallpapers>' >> $themefile
+echo '  <wallpaper deleted="false">' >> $themefile
+echo '    <name>'$name'</name>' >> $themefile
+echo '    <filename>'$lightwp'</filename>' >> $themefile
+echo '    <filename-dark>'$darkwp'</filename-dark>' >> $themefile
+echo '    <options>zoom</options>' >> $themefile
+echo '    <shade_type>solid</shade_type>' >> $themefile
+echo '    <pcolor>#241f31</pcolor>' >> $themefile
+echo '    <scolor>#000000</scolor>' >> $themefile
+echo '  </wallpaper>' >> $themefile
+echo '</wallpapers>' >> $themefile
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Wallpaper Theme Creator for Gnome
+
+Choose two wallpapers to create a simple theme which uses one for the light theme and the other for the dark theme.
+
+## Usage
+
+```sh
+./create_wallpaper_theme.sh -n theme_name -l path/light_theme_image.jpeg -d path/dark_theme_image.png
+```
+
